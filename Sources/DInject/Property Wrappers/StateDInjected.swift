88c6b9b05947165d62826289f6375ccfd9d1b11e
Aung Ko Min <jonahaung@gmail.com>
2024-02-23 01:11:15 +0800

    Update StateDInjected.swift

--- Sources/DInject/Property Wrappers/StateDInjected.swift
+++ Sources/DInject/Property Wrappers/StateDInjected.swift
@@ -15,7 +15,7 @@ public struct StateDInjected<Value> {
     
      public init(_ keyPath: KeyPath<DependencyLabel, Value.Type>? = nil) {
         self.keyPath = keyPath
-        @Environment(\.dependencies) private var dependencies
+        @Environment(\.dependencies) var dependencies
         
         guard let service: Value = dependencies.resolve(for: keyPath) else {
             fatalError("Service \(Value.self) not registered! See logs for more information")
