78464895e8f23dab0e9d6363427c345cd9e2d3be
Aung Ko Min <jonahaung@gmail.com>
2024-02-23 01:05:01 +0800

    Update StateDInjected.swift

--- Sources/DInject/Property Wrappers/StateDInjected.swift
+++ Sources/DInject/Property Wrappers/StateDInjected.swift
@@ -8,14 +8,14 @@
 import SwiftUI
 
 @propertyWrapper
-public final class StateDInjected<Value> {
+public struct StateDInjected<Value> {
     private let keyPath: KeyPath<DependencyLabel, Value.Type>?
-    
+    @Environment(\.dependencies) private var dependencies
     @State public var wrappedValue: Value
     
     public init(_ keyPath: KeyPath<DependencyLabel, Value.Type>? = nil) {
         self.keyPath = keyPath
-        let dependencies = Environment(\.dependencies).wrappedValue
+        
         
         guard let service: Value = dependencies.resolve(for: keyPath) else {
             fatalError("Service \(Value.self) not registered! See logs for more information")
